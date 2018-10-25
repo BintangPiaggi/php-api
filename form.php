890aec1f545c167eb6da7ecd8eb5f1b68899c7a6
Bintang Piaggi Putra <bintang.piaggi44@gmail.com>
2018-10-25 08:32:50 +0700

    second commit

---
+++ form.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+	<meta charset="utf-8">
+	<title>Bintang Piaggi Putra</title>
+</head>
+<body>
+	<form method="post" action="simpan.php">
+		<table>
+			<tr>
+				<td>ID</td>
+				<td><input type="text" name="id"></td>
+			</tr>
+			<tr>
+				<td>Username</td>
+				<td><input type="text" name="id"></td>
+			</tr>
+			<tr>
+				<td>Password</td>
+				<td><input type="text" name="id"></td>
+			</tr>
+			<tr>
+				<td style="vertical-align: top">level</td>
+				<td>
+					<input type="radio" name="level" value="admin">Admin<br>
+					<input type="radio" name="level" value="user">User<br>
+					<input type="radio" name="level" value="guest">Guest<br>
+				</td>
+			</tr>
+			<tr>
+				<td>Fullname</td>
+				<td><input type="text" name="simpan">Simpan</button></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
